01b6779c6edcf21d32ae0aedd8d3d6e1559c7035
jstraughter1013 <support@myvibejournal.com>
2025-04-28 01:41:52 -0700

    feat: add HelloCard demo component

---
+++ src/components/HelloCard.tsx
@@ -0,0 +1,6 @@
+const HelloCard = () => (
+  <div style={{ display: "flex", height: "100%", alignItems: "center", justifyContent: "center", fontSize: 24 }}>
+    👋 Hello from Grid Labs
+  </div>
+);
+export default HelloCard;
